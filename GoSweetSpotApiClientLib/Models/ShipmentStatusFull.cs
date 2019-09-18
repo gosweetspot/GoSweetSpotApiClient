c78e2ccfede2f74b0e5307c2b5cbbc76dfec38a0
Don <don@gosweetspot.com>
2019-09-18 14:10:11 +1200

    Update  shipmentstatus to shipmentstatusfull

--- GoSweetSpotApiClientLib/Models/ShipmentStatusFull.cs
+++ GoSweetSpotApiClientLib/Models/ShipmentStatusFull.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GoSweetSpotApiClientLib.Models
 {
-    public class ShipmentStatus
+    public class ShipmentStatusFull
     {
         public ShipmentStatus()
         {
